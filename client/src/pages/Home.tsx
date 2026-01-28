eae70b69af81a2c22ed2ab2baef4e5751a1e36a4
Lucas Bonnet <lucas.bonnet@epita.fr>
2026-01-29 00:31:54 +0100

    feat(client): adding userProfile type & data fetching

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,28 @@
 import Stack from "@mui/material/Stack";
+import { useEffect } from "react";
 import "./styles/home.css";
 
 const Home = () => {
+  const getPlayerProfile = async (name: string) => {
+    const response = await fetch(`https://open.faceit.com/data/v4/players?nickname=${name}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + import.meta.env.VITE_FACEIT_API_KEY,
+      },
+    });
+    const playerProfile = await response.json();
+    console.log(playerProfile);
+    return playerProfile;
+  };
+
+  useEffect(() => {
+    getPlayerProfile("SwaminG");
+  });
+
   return (
-    <Stack id="home">
-        <div>TEST</div>
+    <Stack id="home-wrapper">
+      <Stack id="home"></Stack>
     </Stack>
   );
 };
---
+++ client/src/user/user.ts
@@ -0,0 +1,11 @@
+export interface UserStat {
+    player_id: string;
+    nickname: string;
+    steam_nickname: string;
+    avatar: string;
+    country: string;
+    activated_at: Date;
+    faceit_url: string;
+    friends_ids: string[];
+    verified: boolean;
+}
