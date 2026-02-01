373e9cd68bd35f68948c66f5ad4b8921fb002b28
Lucas Bonnet <lucas.bonnet@epita.fr>
2026-02-01 01:45:43 +0100

    feat(client): missing files

---
+++ client/src/utils/utils.ts
@@ -0,0 +1,14 @@
+export const levelColors = {
+  lame: "#95a5a6",
+  nice: "#27ae60",
+  good: "#e67e22",
+  veryGood: "#d35400",
+  excellent: "#c0392b",
+};
+
+export const levelColorIndicator = Array(2)
+  .fill("#95a5a6")
+  .concat(Array(2).fill("#27ae60"))
+  .concat(Array(3).fill("#e67e22"))
+  .concat(Array(2).fill("#d35400"))
+  .concat(["#c0392b"]);
